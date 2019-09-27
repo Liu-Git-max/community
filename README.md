787f57554c1ac8d9b7dbe2217611a052df912837
Liu-Git-max <403049042@qq.com>
2019-09-27 19:36:48 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ http://localhost:8887
 ## 资料
 [Spring 文档](https://spring.io/guides)    
 [Spring Web](https://spring.io/guides/gs/serving-web-content/)   
-[es](https://elasticsearch.cn/explore)    
+[社区](https://elasticsearch.cn/explore)    
 [Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)    
 [Bootstrap](https://v3.bootcss.com/getting-started/)    
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)    
---
+++ src/main/java/community/community/controller/AuthorizeController.java
@@ -0,0 +1,35 @@
+package community.community.controller;
+
+import community.community.dto.AccessTokenDTO;
+import community.community.dto.GithubUser;
+import community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("11f1dedc89a24efa2581");
+        accessTokenDTO.setClient_secret("3259152e782140a6d74efca54aab05468ff067ab");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
