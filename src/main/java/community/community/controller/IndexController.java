9998e398c7e47bd3fe0c64975c74034122a75885
Liu-Git-max <403049042@qq.com>
2019-09-27 10:40:15 +0800

    add bootstrap

--- src/main/java/community/community/controller/IndexController.java
+++ src/main/java/community/community/controller/IndexController.java
@@ -5,11 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name1") String name, Model model){
-        model.addAttribute("name1",name);
-        return "hello1";
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
