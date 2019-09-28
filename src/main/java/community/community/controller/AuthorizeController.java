a7854ec0d6a9531d28ac098368ad752612b4c234
Liu-Git-max <403049042@qq.com>
2019-09-28 22:18:56 +0800

    add  check cookies

--- src/main/java/community/community/controller/AuthorizeController.java
+++ src/main/java/community/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -27,7 +29,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                            HttpServletRequest request) throws IOException {
+                           HttpServletRequest request, HttpServletResponse response) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("11f1dedc89a24efa2581");
         accessTokenDTO.setClient_secret("3259152e782140a6d74efca54aab05468ff067ab");
@@ -38,15 +40,17 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
 
         if (githubUser != null){
-            //登陆成功，写cookie和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
+            //登陆成功，写cookie和session
+
             return "redirect:/";
         }else {
             //登录失败，重新登陆
